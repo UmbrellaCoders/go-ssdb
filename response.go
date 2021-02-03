0a87572d390572215b677b80e16b44e2d5d8f3dd
NG Ads <just.ng.ads@gmail.com>
2021-02-04 01:11:45 +0300

    toBool fix

--- response.go
+++ response.go
@@ -1,8 +1,8 @@
 package ssdb
 
 import (
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type response struct {
@@ -11,14 +11,17 @@ type response struct {
 	packages       []string
 }
 
-func (r *response) toString() (string) {
+func (r *response) toString() string {
 	return strings.Join(r.packages, "")
 }
-func (r *response) toBool() (bool) {
+func (r *response) toBool() bool {
 	if r.responseStatus {
 		b := true
 		rsize := len(r.packages)
-		if rsize > 0&&r.packages[0] == "false" {
+		if rsize > 0 && r.packages[0] == "false" {
+			b = false
+		}
+		if rsize > 0 && r.packages[0] == "0" {
 			b = false
 		}
 		return b
@@ -27,33 +30,33 @@ func (r *response) toBool() (bool) {
 	}
 
 }
-func (r *response) toInt() (int64) {
+func (r *response) toInt() int64 {
 	if i, err := strconv.ParseInt(r.packages[0], 10, 64); err == nil {
 		return i
 	} else {
 		return 0
 	}
 }
-func (r *response)toFloat() (float64) {
+func (r *response) toFloat() float64 {
 	if i, err := strconv.ParseFloat(r.packages[0], 64); err == nil {
 		return i
 	} else {
 		return 0
 	}
 }
-func (r *response) toArray() ([]string) {
+func (r *response) toArray() []string {
 	//todo should return new array fill with r.packages
 	return r.packages
 }
-func (r *response) toMap() (map[string]string) {
+func (r *response) toMap() map[string]string {
 
 	mapData := make(map[string]string)
 	size := len(r.packages)
 	i := 0
-	if r.responseStatus&&size % 2 == 0 {
+	if r.responseStatus && size%2 == 0 {
 		for i < size {
 			key := r.packages[i]
-			value := r.packages[i + 1]
+			value := r.packages[i+1]
 			mapData[key] = value
 			i += 2
 		}
@@ -61,10 +64,10 @@ func (r *response) toMap() (map[string]string) {
 
 	return mapData
 }
-func newSSDBResponse() (*response) {
+func newSSDBResponse() *response {
 	return &response{
-		responseStatus:true,
-		responseText:"ok",
-		packages:nil,
+		responseStatus: true,
+		responseText:   "ok",
+		packages:       nil,
 	}
 }
